cdd1af32bc95466d58cbee232682af6abc12bf96
Ritwik Shaw <pluto@Dotcom.local>
2020-07-25 18:37:44 +0530

    first

---
+++ client/src/components/view/view.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Post from '../post/post'
+import { Redirect, Link } from 'react-router-dom'
+import ContextUser from '../../contextUser'
+
+// container component for viewing a single picture
+class View extends Component {
+    static contextType = ContextUser;
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            redirect_loc: undefined,
+            redirect: false
+        }
+    }
+
+    updateRedirect = (bool, location) => {
+        this.setState({redirect: bool, redirect_loc: location})
+    }
+
+
+
+    render() {
+        if (this.state.redirect && this.state.redirect_loc == "error") {
+            return <Redirect to={`/error`}/>
+        } else if (this.state.redirect && this.state.redirect_loc == "profile") {
+            return <Redirect to={`/u/${this.context.state.logged}`}/>
+        } else {
+            return (
+            <div className="view">
+                <Post updateRedirect={this.updateRedirect} post_id={this.props.match.params.id}/>
+            </div>
+            )
+        }
+    }
+}
+
+View.contextType = ContextUser;
+
+
+export default View;
